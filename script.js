e770287a2dd3f4b285278c94f2a826b884f0399f
donizeti_silva <61122214+donizeti26@users.noreply.github.com>
2024-04-24 22:30:09 -0300

    pegando dados do usuario e executando operações com eval

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-let num = document.querySelectorAll('.num')
+let numero =[]
 
 
-for(let c in num){
-    console.log(num[c])
+
+function insert(num){
+    let visor = document.querySelector('#visor-content')        
+    visor.innerHTML+=`${num}`
+}
+function limpar(){
+    let visor = document.querySelector('#visor-content')        
+    visor.innerHTML=""
+    
+}
+function porct(num){
+    
+}
+function delet(){
+    let visor = document.querySelector('#visor-content')
+    visor.innerHTML = visor.slice(0, -1)
+    
+
+}
+function calcular(){
+    let visor = document.querySelector('#visor-content')
+
+    if(visor){
+        let calculo = visor.innerText
+        visor.innerHTML = eval(calculo)
+    }else{
+        visor.innerHTML = 'informe um n º'
+
+    }
 }
